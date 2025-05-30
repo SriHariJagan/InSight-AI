91c8c838a3a660e4f3a52da462e2982fa1ad5a3b
Sri Hari Jagan <100404902+SriHariJagan@users.noreply.github.com>
2025-05-30 15:25:21 +0530

    Add files via upload

--- src/Components/ChatContainer/ChatContainer.jsx
+++ src/Components/ChatContainer/ChatContainer.jsx
@@ -3,12 +3,22 @@ import styles from "./ChatContainer.module.css";
 import ChatHeader from "../ChatHeader/ChatHeader";
 import InputArea from "../InputArea/InputArea";
 import Messages from "../Message/Messages";
+import { useTheme } from "../../Store/Context/ThemeContext";
 
 
 export default function ChatContainer() {
 
+  const { toggleSidebar, showSidebar } = useTheme()
+
+  const handlesidebar = () => {
+    if(showSidebar){
+      toggleSidebar()
+    }
+  }
+
+
   return (
-    <div className={styles.chatContainer}>
+    <div className={styles.chatContainer} onClick={handlesidebar}>
       <ChatHeader />
       <Messages />
       <InputArea />
